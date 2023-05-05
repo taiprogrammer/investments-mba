2ef82948db8f5930f69cb50f44cb8e26c694f300
taiprogrammer <taiza.marques@sptech.school>
2023-05-05 01:25:50 -0300

    calling api

--- src/pages/components/actions/index.jsx
+++ src/pages/components/actions/index.jsx
@@ -1,75 +1,31 @@
-import style from "./Actions.module.css";
-export function Actions() {
+import style from "../Reports.module.css";
+export function Actions({ investments, reports }) {
+  console.log(reports);
   return (
     <>
       <section className={style.container}>
         <header>
-          <h3>Fundo de Ações</h3>
+          <h3>{investments.description}</h3>
           <h4>
-            Rendimento total de <span className={style.status}>R$ 614,54 (+61,45%)</span>
+            Rendimento total de{" "}
+            <span className={style.status}>R$ 614,54 (+61,45%)</span>
           </h4>
         </header>
         <main className={style.conatiner_info}>
-          <div className={style.info}>
-            <p>jan/2020</p>
-            <p>R$ 1.100,00</p>
-            <p>0,00%</p>
-          </div>
-          <div className={style.info}>
-            <p>jan/2020</p>
-            <p>R$ 1.100,00</p>
-            <p>0,00%</p>
-          </div>
-          <div className={style.info}>
-            <p>jan/2020</p>
-            <p>R$ 1.100,00</p>
-            <p>0,00%</p>
-          </div>
-          <div className={style.info}>
-            <p>jan/2020</p>
-            <p>R$ 1.100,00</p>
-            <p>0,00%</p>
-          </div>
-          <div className={style.info}>
-            <p>jan/2020</p>
-            <p>R$ 1.100,00</p>
-            <p>0,00%</p>
-          </div>
-          <div className={style.info}>
-            <p>jan/2020</p>
-            <p>R$ 1.100,00</p>
-            <p>0,00%</p>
-          </div>
-          <div className={style.info}>
-            <p>jan/2020</p>
-            <p>R$ 1.100,00</p>
-            <p>0,00%</p>
-          </div>
-          <div className={style.info}>
-            <p>jan/2020</p>
-            <p>R$ 1.100,00</p>
-            <p>0,00%</p>
-          </div>
-          <div className={style.info}>
-            <p>jan/2020</p>
-            <p>R$ 1.100,00</p>
-            <p>0,00%</p>
-          </div>
-          <div className={style.info}>
-            <p>jan/2020</p>
-            <p>R$ 1.100,00</p>
-            <p>0,00%</p>
-          </div>
-          <div className={style.info}>
-            <p>jan/2020</p>
-            <p>R$ 1.100,00</p>
-            <p>0,00%</p>
-          </div>
-          <div className={style.info}>
-            <p>jan/2020</p>
-            <p>R$ 1.100,00</p>
-            <p>0,00%</p>
-          </div>
+          {reports.length > 0 &&
+            reports.map((item) => {
+              if (item.investmentId === investments.id) {
+                return (
+                  <div className={style.info}>
+                    <p>
+                      {item.month}/{item.year}
+                    </p>
+                    <p>R$ {item.value}</p>
+                    <p>0,00%</p>
+                  </div>
+                );
+              }
+            })}
         </main>
       </section>
     </>
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,13 +1,35 @@
 import style from "./Home.module.css";
-import { Cripto } from "../components/cripto";
 import { Actions } from "../components/actions";
+import { useState } from "react";
+import { api } from "../../lib/axios";
+import { useEffect } from "react";
 
 export function Home() {
+  const [investments, setInvestments] = useState([]);
+  const [reports, setReports] = useState([]);
+
+  function getInvestments() {
+    api.get("/investments").then(async ({ data }) => {
+      setInvestments(await data);
+    });
+  }
+
+  function getReports() {
+    api.get("/reports").then(async ({ data }) => {
+      setReports(await data);
+    });
+  }
+
+  useEffect(() => {
+    getInvestments();
+    getReports();
+  }, []);
   return (
     <>
       <div className={style.container}>
-        <Actions />
-        <Cripto />
+        {investments.map((item) => {
+          return <Actions investments={item} reports={reports} key={item.id} />;
+        })}
       </div>
     </>
   );
